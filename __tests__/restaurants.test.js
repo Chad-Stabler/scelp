edf9c7caafa1ca089f6436e2981743685ab57a8e
Chad Stabler <stablerpsn@gmail.com>
2022-08-16 14:22:57 -0500

    get by id for restaurant route working

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -9,10 +9,15 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should return a list of resaurants with detail', async () => {
+  it('should return a list of restaurants with detail', async () => {
     const resp = await request(app).get('/api/v1/restaurants');
     expect(resp.status).toBe(200);
     expect(resp.body.length).toBe(4);
+  });
+  it('should return a restaurant with nested reviews', async () => {
+    const resp = await request(app).get('/api/v1/restaurants/1');
+    expect(resp.body.name).toBe('restaurant A');
+    expect(resp.status).toBe(200);
   });
   afterAll(() => {
     pool.end();
